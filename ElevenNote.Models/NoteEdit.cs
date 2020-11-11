43e38276d9aa10a8ad3ced0631294e912065bffe
Austin <ausjwill@gmail.com>
2020-11-11 15:39:20 -0500

    star

--- ElevenNote.Models/NoteEdit.cs
+++ ElevenNote.Models/NoteEdit.cs
@@ -9,7 +9,11 @@ namespace ElevenNote.Models
     public class NoteEdit
     {
         public int NoteId { get; set; }
+
         public string Title { get; set; }
+
         public string Content { get; set; }
+
+        public bool IsStarred { get; set; }
     }
 }
